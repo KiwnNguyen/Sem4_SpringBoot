0e04d3f19653f38443daf5ee4d54ab84d0175017
KiwnNguyen <nguyenduytan18.work@gmail.com>
2023-09-11 09:54:32 +0700

    khởi tạo project

---
+++ sb_bai2/src/main/java/com/example/bai2/SbBai2Application.java
@@ -0,0 +1,13 @@
+package com.example.bai2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbBai2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbBai2Application.class, args);
+	}
+
+}
